686cf9a7a20a831fd63837812271dcc466b5321f
isobelbutler <isobutler@icloud.com>
2023-10-03 17:30:26 +0100

    Change bookpage.js and added pre-authentication consts

--- src/routes/bookpage.js
+++ src/routes/bookpage.js
@@ -1,13 +1,22 @@
 const express = require('express');
-//Variables
 const router = express.Router();
+
+const { getSession } = require("../model/session.js");
 
 // const templates = require("../templates");
 
 router.get('/:user_id', (req, res) => {
   // Get the user ID from the URL parameters
-  const userId = req.params.user_id;
-  res.send(`<h1>Welcome to your bookshelf, User ID: ${userId}</h1>`);
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid)
+  const current_user = req.session && req.session.user_id;
+  const bookshelf_owner = Number(req.params.user_id);
+
+  // const page_owner = Number(req.params.user_id);
+  // if (current_user !== page_owner) {
+  //   return res.status(401).send("<h1>You aren't allowed to see that</h1>");
+  // }
+  res.send(`<h1>Welcome to your bookshelf, User ID: ${bookshelf_owner}</h1>`);
 });
 
 module.exports = router;
